f10ae90abcb8be57e6caf3a4332956c996417510
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-06-26 19:19:19 +0530

    navlogo setted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
-
-function App() {
+import styled, {ThemeProvider} from "styled-components";
+import {darkTheme} from "./data/utils/Themes.js";
+import Navbar from "./components/Navbar";
+import {BrowserRouter} from "react-router-dom";
+function App(){
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <BrowserRouter>
+                <Navbar />
+            </BrowserRouter>
+            
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+import {Link as LinkR} from "react-router-dom";
+
+const NavbarContainer = styled.div`
+    background-color:${({theme}) => theme.bg};
+    height:80px;
+    display:flex;
+    align-items: center;
+    justify-content:center;
+    font-size:1rem;
+    position:sticky;
+    top:0;
+    z-index:10;
+    color:white;
+`;
+const NavLogo = styled(LinkR)`
+    padding:0 6px;
+    text-decoration:none;
+    color:inherit;
+`;
+
+const Navbar = () => {
+  return <NavbarContainer>
+    <NavLogo to="/">Keerthi</NavLogo>
+  </NavbarContainer>;
+};
+
+export default Navbar;
