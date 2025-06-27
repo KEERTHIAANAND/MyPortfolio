59aeee6f626aca35175a8c40aa7d8955c9cd9037
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-06-27 17:22:11 +0530

    navbar initiated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,64 @@ import styled from "styled-components";
 import {Link as LinkR} from "react-router-dom";
 
 const NavbarContainer = styled.div`
-    background-color:${({theme}) => theme.bg};
-    height:80px;
-    display:flex;
+  background-color: #000; /* true black */
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  padding: 0 40px;
+  height: 60px;
+  font-family: 'Montserrat', sans-serif;
+  border: none;
+  border-bottom: 1px solid #000; /* for black line if you want */
+`;
+
+
+const NavLogo = styled(LinkR)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 1.2rem;
+  cursor: pointer;
+`;
+
+
+const NavItems = styled.ul`
+    width:100%;
+    display: flex;
     align-items: center;
     justify-content:center;
-    font-size:1rem;
-    position:sticky;
-    top:0;
-    z-index:10;
-    color:white;
-`;
-const NavLogo = styled(LinkR)`
+    gap:32px;
     padding:0 6px;
+    list-style:none;
+`;
+
+const NavLink = styled.a`
+    color: ${({theme}) => theme.text_primary};
+    font-weight:500;
+    cursor:pointer;
+    transition: all 0.2s ease-in-out;
     text-decoration:none;
-    color:inherit;
+    &:hover{
+      color:${({theme}) => theme.primary};
+    }
 `;
 
 const Navbar = () => {
   return <NavbarContainer>
     <NavLogo to="/">Keerthi</NavLogo>
+
+    <NavItems>
+      <NavLink href="#About">About</NavLink>
+      <NavLink href="#Skills">Skills</NavLink>
+      <NavLink href="#Experience">Experience</NavLink>
+      <NavLink href="#Projects">Projects</NavLink>
+      <NavLink href="#Education">Education</NavLink>
+    </NavItems>
+
   </NavbarContainer>;
 };
 
